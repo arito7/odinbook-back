86184bfd11e2a0139ea9dadcdd02c139545526bd
arito7 <64824933+arito7@users.noreply.github.com>
2022-05-04 12:17:52 +0900

    Add GET posts route

---
+++ routes/posts.js
@@ -0,0 +1,65 @@
+const Post = require('../models/Post');
+const User = require('../models/User.js');
+const { Router } = require('express');
+
+const {
+  postValidation,
+  validateResults,
+} = require('../config/validationSchemas');
+
+const posts = Router();
+
+/**
+ * Root is /posts
+ */
+
+posts.post('/', postValidation, validateResults, (req, res, next) => {
+  if (req.user) {
+    const post = new Post({
+      creator: req.user._id,
+      body: req.body.body,
+    });
+
+    post.save((err, savedPost) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Database error.',
+          error: err.message,
+        });
+      }
+      res.status(200);
+      res.json({
+        success: true,
+        message: 'Post created successfully.',
+        post: savedPost,
+      });
+    });
+  } else {
+    res.redirect('/unauthorized');
+  }
+});
+
+/**
+ * currently only gets requesting users posts.
+ * ultimately needs to be able to get all posts of friends of the
+ * requesting user as well
+ */
+posts.get('/', (req, res) => {
+  if (req.user) {
+    Post.find({ creator: req.user._id })
+      .populate('creator', 'username')
+      .sort({ createdAt: -1 })
+      .limit(50)
+      .exec((err, posts) => {
+        if (err) {
+          console.log(err.message);
+        }
+        return res.json({ success: true, posts });
+      });
+  } else {
+    res.redirect('/unauthorized');
+  }
+});
+
+module.exports = posts;
