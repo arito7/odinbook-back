b0f331f0f2b636fe8c73330c9191bb974061efa2
Yuji T <accmanager1990@gmail.com>
2022-05-08 22:29:18 +0900

    Add more res object creators

--- helpers/resObjects.js
+++ helpers/resObjects.js
@@ -1,7 +1,24 @@
-exports.createDBErrorRes = (err) => ({
+module.exports.createDBErrorRes = (err) => ({
   success: false,
   message: 'Database Error',
   error:
     err.message ||
     `Error occurred while parsing ${err}, make sure you are passing an Error object`,
+});
+
+module.exports.createGenericRes = (successBoolean, message) => ({
+  success: successBoolean,
+  message,
+});
+
+module.exports.createFailRes = (message, error = null) => ({
+  success: false,
+  message,
+  error,
+});
+
+module.exports.createSuccessRes = (message, dataObj = null) => ({
+  success: true,
+  message,
+  ...dataObj,
 });
