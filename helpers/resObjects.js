c41b5d07a12883f7e3d3b7c7b6f5150ec6b4861b
Yuji T <accmanager1990@gmail.com>
2022-05-08 15:25:23 +0900

    Add helpful message for errors

--- helpers/resObjects.js
+++ helpers/resObjects.js
@@ -1,5 +1,7 @@
 exports.createDBErrorRes = (err) => ({
   success: false,
   message: 'Database Error',
-  error: err.message || 'Error parsing error',
+  error:
+    err.message ||
+    `Error occurred while parsing ${err}, make sure you are passing an Error object`,
 });
